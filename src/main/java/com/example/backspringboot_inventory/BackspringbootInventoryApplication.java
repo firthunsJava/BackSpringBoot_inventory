5f3b4d06df6cea24c9183f2f1dd7eb802285e8de
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-07-30 10:12:20 +0200

    Initial commit

---
+++ src/main/java/com/example/backspringboot_inventory/BackspringbootInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.backspringboot_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackspringbootInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackspringbootInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backspringboot_inventory/BackspringbootInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backspringboot_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackspringbootInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
