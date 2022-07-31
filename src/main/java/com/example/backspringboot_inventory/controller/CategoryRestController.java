443850ea530849b7773cf4a55f63222692fade11
Firthuns2021 <86574425+Firthuns2021@users.noreply.github.com>
2022-07-31 20:09:37 +0200

    CRUD COMPLETO REALIZADO

--- src/main/java/com/example/backspringboot_inventory/controller/CategoryRestController.java
+++ src/main/java/com/example/backspringboot_inventory/controller/CategoryRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@CrossOrigin(origins = {"http://localhost:4200"}) // cors, que mi servidor me permite la transmisión de datos
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
